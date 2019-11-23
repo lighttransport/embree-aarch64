2569d34c77a9c39de3887622623418a0c9ecb620
Syoyo Fujita <syoyo@lighttransport.com>
2019-11-23 19:30:54 +0900

    Android build test using Github Actions.

--- scripts/bootstrap-android-cmake-linux.sh
+++ scripts/bootstrap-android-cmake-linux.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
 # Edit path to Android NDK
+
+# Use ANDROID_NDK_HOME environment
+ANDROID_NDK_ROOT=$ANDROID_NDK_HOME
+
 # default installation directory of NDK through Android Studio
-ANDROID_NDK_ROOT=$HOME/Android/Sdk/ndk-bundle
+# ANDROID_NDK_ROOT=$HOME/Android/Sdk/ndk-bundle
+
+# Set your own path
 #ANDROID_NDK_ROOT=$HOME/local/android-ndk-r16b/
 
 # CMake 3.6 or later required.
-CMAKE_BIN=$HOME/local/cmake-3.12.4-Linux-x86_64/bin/cmake
+CMAKE_BIN=cmake
 
-#CMAKE_BIN=cmake
 rm -rf build-android
+mkdir build-android
+cd build-android
 
-$CMAKE_BIN -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake \
+$CMAKE_BIN -G Ninja -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake \
   -DANDROID_ABI=arm64-v8a \
   -DANDROID_NATIVE_API_LEVEL=24 \
   -DANDROID_ARM_MODE=arm \
   -DANDROID_ARM_NEON=TRUE \
   -DANDROID_STL=c++_shared \
+  -DEMBREE_ARM=On \
   -DEMBREE_ISPC_SUPPORT=Off \
   -DEMBREE_TASKING_SYSTEM=Internal \
   -DEMBREE_TUTORIALS=Off \
   -DEMBREE_MAX_ISA=SSE2 \
   -DEMBREE_RAY_PACKETS=Off \
-  -Bbuild-android -H.
+  ..
+
+cd ..
