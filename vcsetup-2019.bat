26cc66a480285498069f7fb619978e1a007f0eeb
Syoyo Fujita <syoyo@lighttransport.com>
2020-03-09 01:42:58 +0900

    Add Windows(x64) + VS2019 CI build

---
+++ vcsetup-2019.bat
@@ -0,0 +1,12 @@
+rmdir /q /s build
+mkdir build
+cd build
+
+cmake -G "Visual Studio 16 2019" -A x64 ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DEMBREE_ISPC_SUPPORT=Off ^
+  -DEMBREE_TASKING_SYSTEM=Internal ^
+  -DEMBREE_TUTORIALS=Off ^
+  -DEMBREE_MAX_ISA=SSE2 ^
+  -DEMBREE_RAY_PACKETS=Off ^
+  ..
