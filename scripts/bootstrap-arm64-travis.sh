a14bc672664daff3ed8a64826a6eb532dafaa26b
Syoyo Fujita <syoyo@lighttransport.com>
2020-10-14 12:27:38 +0900

    Disable ASAN for arm64 travis build for a while.

--- scripts/bootstrap-arm64-travis.sh
+++ scripts/bootstrap-arm64-travis.sh
@@ -10,18 +10,19 @@ mkdir build-aarch64
 cd build-aarch64
 
 # CC and CXX are set by Travis
+# Disable ASAN for a while to avoid Travis job timeout when running `verify` test.
 
 $CMAKE_BIN \
   -G Ninja \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DEMBREE_ARM=On \
-  -DEMBREE_ADDRESS_SANITIZER=On \
+  -DEMBREE_ADDRESS_SANITIZER=Off \
   -DCMAKE_INSTALL_PREFIX=$HOME/local/embree3 \
   -DEMBREE_ISPC_SUPPORT=Off \
   -DEMBREE_TASKING_SYSTEM=Internal \
   -DEMBREE_TUTORIALS=Off \
   -DEMBREE_MAX_ISA=SSE2 \
-  -DEMBREE_RAY_PACKETS=On \
+  -DEMBREE_RAY_PACKETS=Off \
   ..
 
 cd ..
