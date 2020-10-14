a121d91bc2bf5b384689d0bb3dfab85b31ec4c88
Syoyo Fujita <syoyo@lighttransport.com>
2020-10-14 13:15:24 +0900

    Support build variants for Travis CI build.

--- scripts/bootstrap-arm64-travis.sh
+++ scripts/bootstrap-arm64-travis.sh
@@ -4,6 +4,14 @@
 
 CMAKE_BIN=cmake
 
+# -- read config fron environment variable(if defined)
+
+MAX_ISA=${ENV_MAX_ISA:-SSE2}
+RAY_PACKETS=${ENV_RAY_PACKETS:-Off}
+NEON_AVX2_EMULATION=${ENV_NEON_AVX2_EMULATION:-Off}
+
+# --------------
+
 rm -rf build-aarch64
 mkdir build-aarch64
 
@@ -21,8 +29,9 @@ $CMAKE_BIN \
   -DEMBREE_ISPC_SUPPORT=Off \
   -DEMBREE_TASKING_SYSTEM=Internal \
   -DEMBREE_TUTORIALS=Off \
-  -DEMBREE_MAX_ISA=SSE2 \
-  -DEMBREE_RAY_PACKETS=Off \
+  -DEMBREE_MAX_ISA=${MAX_ISA} \
+  -DEMBREE_RAY_PACKETS=${RAY_PACKETS} \
+  -DEMBREE_NEON_AVX2_EMULATION=${NEON_AVX2_EMULATIONON} \
   ..
 
 cd ..
